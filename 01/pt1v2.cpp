d03b0516d143aa229eb1a6450a970f7fe5d10095
Avi <avi.ron012@gmail.com>
2022-12-01 23:55:56 +0200

    now in cpp

---
+++ 01/pt1v2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    string line;
+    vector<int> sums{};
+    while(getline(cin, line)){
+        if(line.length()){
+            sum += stoi(line);
+        }else{
+            sums.push_back(sum);
+            sum = 0;
+        }
+    }
+    sort(sums.begin(),sums.end(), greater<int>());
+    cout << sums[0];
+}
