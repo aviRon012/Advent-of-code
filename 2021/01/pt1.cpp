45da2f10fd5202e20825623222b8f8eb6d11ee1b
Avi <avi.ron012@gmail.com>
2022-12-03 00:55:55 +0200

    21/01 template

--- 2021/01/pt1.cpp
+++ 2021/01/pt1.cpp
@@ -1,13 +1,15 @@
-#include "..\..\clipboard.hpp"
-#include <vector>
+#include <windows.h>
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
-int main(){
+int expected = 7;
+
+int handleLines(vector<string> &lines){
     vector<int> nums;
-    string line;
-    while(getline(cin, line)){
+    for(auto &line: lines){
         nums.push_back(stoi(line));
     }
     int len = nums.size() - 1;
@@ -15,6 +17,46 @@ int main(){
     for(int i = 0; i < len; i++){
         count += (nums[i] < nums[i + 1]);
     }
-    cout << count;
-    copyToClipBoard(count);
+    return count;
+}
+
+int handleFile(const string &path){
+    fstream file;
+    file.open(path, ios::in);
+    if(!file.is_open()){
+        cout << "\33[31mCan't open file: " << path << "\33[39m\n";
+        return -1;
+    }
+    vector<string> lines;
+    string line;
+    while(getline(file, line)) lines.push_back(line);
+    file.close();
+    return handleLines(lines);
+}
+
+void clipboard(int64_t num){
+    string str = to_string(num);
+    const size_t len = str.length() + 1;
+    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), str.c_str(), len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+}
+
+int main()
+{
+    int result;
+    result = handleFile("example.txt");
+    cout << result;
+    if(result == expected){
+        cout << " \33[32m[OK]\33[39m\n";
+    }else{
+        cout << " != " << expected << " \33[31m[FAIL]\33[39m\n";
+    }
+    result = handleFile("input.txt");
+    cout << result;
+    clipboard(result);
 }
--- 2021/01/pt2.cpp
+++ 2021/01/pt2.cpp
@@ -1,17 +1,19 @@
-#include "..\..\clipboard.hpp"
-#include <vector>
+#include <windows.h>
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
-int main(){
+int expected = 5;
+
+int handleLines(vector<string> &lines){
     vector<int> nums;
-    vector<int> sums;
-    string line;
-    while(getline(cin, line)){
+    for(auto &line: lines){
         nums.push_back(stoi(line));
     }
     int len = nums.size() - 2;
+    vector<int> sums;
     for(int i = 0; i < len; i++){
         sums.push_back(nums[i] + nums[i + 1] + nums[i + 2]);
     }
@@ -20,6 +22,46 @@ int main(){
     for(int i = 0; i < len; i++){
         count += (sums[i] < sums[i + 1]);
     }
-    cout << count;
-    copyToClipBoard(count);
+    return count;
+}
+
+int handleFile(const string &path){
+    fstream file;
+    file.open(path, ios::in);
+    if(!file.is_open()){
+        cout << "\33[31mCan't open file: " << path << "\33[39m\n";
+        return -1;
+    }
+    vector<string> lines;
+    string line;
+    while(getline(file, line)) lines.push_back(line);
+    file.close();
+    return handleLines(lines);
+}
+
+void clipboard(int64_t num){
+    string str = to_string(num);
+    const size_t len = str.length() + 1;
+    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), str.c_str(), len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+}
+
+int main()
+{
+    int result;
+    result = handleFile("example.txt");
+    cout << result;
+    if(result == expected){
+        cout << " \33[32m[OK]\33[39m\n";
+    }else{
+        cout << " != " << expected << " \33[31m[FAIL]\33[39m\n";
+    }
+    result = handleFile("input.txt");
+    cout << result;
+    clipboard(result);
 }
