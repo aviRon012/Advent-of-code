25d49f2d62e72aaed2528812e2ee5413a688fcdf
Avi <avi.ron012@gmail.com>
2022-12-03 01:16:02 +0200

    21/03 template

--- 2021/02/pt1.cpp
+++ 2021/02/pt1.cpp
@@ -1,25 +1,66 @@
-#include "..\..\clipboard.hpp"
-#include <string>
+#include <windows.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
-    string str;
-    int x = 0, y = 0, num;
-    while(cin >> str){
-        cin >> num;
-        switch(str[0]){
+int expected = 150;
+
+int handleLines(vector<string> &lines){
+    int x = 0, y = 0;
+    for(auto &line: lines){
+        switch(line[0]){
             case 'u':
-                y -= num;
+                y -= (line[3] - '0');
                 break;
             case 'd':
-                y += num;
+                y += (line[5] - '0');
                 break;
             case 'f':
-                x += num;
+                x += (line[8] - '0');
         }
     }
-    int result = x * y;
+    return x * y;
+}
+
+int handleFile(const string &path){
+    fstream file;
+    file.open(path, ios::in);
+    if(!file.is_open()){
+        cout << "\33[31mCan't open file: " << path << "\33[39m\n";
+        return -1;
+    }
+    vector<string> lines;
+    string line;
+    while(getline(file, line)) lines.push_back(line);
+    file.close();
+    return handleLines(lines);
+}
+
+void clipboard(int64_t num){
+    string str = to_string(num);
+    const size_t len = str.length() + 1;
+    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), str.c_str(), len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+}
+
+int main()
+{
+    int result;
+    result = handleFile("example.txt");
     cout << result;
-    copyToClipBoard(result);
+    if(result == expected){
+        cout << " \33[32m[OK]\33[39m\n";
+    }else{
+        cout << " != " << expected << " \33[31m[FAIL]\33[39m\n";
+    }
+    result = handleFile("input.txt");
+    cout << result;
+    clipboard(result);
 }
--- 2021/02/pt2.cpp
+++ 2021/02/pt2.cpp
@@ -1,26 +1,67 @@
-#include "..\..\clipboard.hpp"
-#include <string>
+#include <windows.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
-int main(){
-    string str;
-    int aim = 0, deapth = 0, horizontal = 0, num;
-    while(cin >> str){
-        cin >> num;
-        switch(str[0]){
+int expected = 900;
+
+int handleLines(vector<string> &lines){
+    int aim = 0, deapth = 0, horizontal = 0;
+    for(auto &line: lines){
+        switch(line[0]){
             case 'u':
-                aim -= num;
+                aim -= (line[3] - '0');
                 break;
             case 'd':
-                aim += num;
+                aim += (line[5] - '0');
                 break;
             case 'f':
-                horizontal += num;
-                deapth += (aim * num);
+                horizontal += (line[8] - '0');
+                deapth += (line[8] - '0') * aim;
         }
     }
-    int result = horizontal * deapth;
+    return horizontal * deapth;
+}
+
+int handleFile(const string &path){
+    fstream file;
+    file.open(path, ios::in);
+    if(!file.is_open()){
+        cout << "\33[31mCan't open file: " << path << "\33[39m\n";
+        return -1;
+    }
+    vector<string> lines;
+    string line;
+    while(getline(file, line)) lines.push_back(line);
+    file.close();
+    return handleLines(lines);
+}
+
+void clipboard(int64_t num){
+    string str = to_string(num);
+    const size_t len = str.length() + 1;
+    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), str.c_str(), len);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+}
+
+int main()
+{
+    int result;
+    result = handleFile("example.txt");
     cout << result;
-    copyToClipBoard(result);
+    if(result == expected){
+        cout << " \33[32m[OK]\33[39m\n";
+    }else{
+        cout << " != " << expected << " \33[31m[FAIL]\33[39m\n";
+    }
+    result = handleFile("input.txt");
+    cout << result;
+    clipboard(result);
 }
