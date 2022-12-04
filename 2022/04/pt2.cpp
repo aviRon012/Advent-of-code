06ed2eaf8d186b72bb794ba76fcc464980da6eb3
Avi <avi.ron012@gmail.com>
2022-12-04 07:38:27 +0200

    22/04

---
+++ 2022/04/pt2.cpp
@@ -0,0 +1,47 @@
+#include "../../aoc-utils.hpp"
+#include <iostream>
+using namespace std;
+using namespace Aoc;
+
+int expected = 4;
+
+bool overlaps(int x0, int x1, int y0, int y1)
+{
+    if(x0 <= y0 && x1 >= y0) return true;
+    if(x0 <= y1 && x1 >= y1) return true;
+    if(y0 <= x0 && y1 >= x0) return true;
+    if(y0 <= x1 && y1 >= x1) return true;
+    return false;
+}
+
+int handleFile(const string &path)
+{
+    auto lines = FileToLines(path);
+    int x0, x1, y0, y1, count = 0, pos;
+    for(auto &line: lines){
+        x0 = stoi(line.substr(0 ,pos = line.find('-')));
+        line.erase(0, pos + 1);
+        x1 = stoi(line.substr(0 ,pos = line.find(',')));
+        line.erase(0, pos + 1);
+        y0 = stoi(line.substr(0 ,pos = line.find('-')));
+        line.erase(0, pos + 1);
+        y1 = stoi(line);
+        count += overlaps(x0, x1, y0, y1);
+    }
+    return count;
+}
+
+int main()
+{
+    int result;
+    result = handleFile("example.txt");
+    cout << result;
+    if(result == expected){
+        cout << " \33[32m[OK]\33[39m\n";
+    }else{
+        cout << " != " << expected << " \33[31m[FAIL]\33[39m\n";
+    }
+    result = handleFile("input.txt");
+    cout << result;
+    clipboard(to_string(result));
+}
