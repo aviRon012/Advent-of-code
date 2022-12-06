59dc3cac9ccf77b45d3670ff2cd9e1bcf14e2742
Avi <avi.ron012@gmail.com>
2022-12-06 09:32:02 +0200

    22/06

---
+++ 2022/06/pt1.cpp
@@ -0,0 +1,42 @@
+#include "../../aoc-utils.hpp"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+using namespace Aoc;
+
+int expected = 7;
+
+bool isUniqe(string &str)
+{
+    sort(ALL(str));
+    for(int j = 1; j < 4; j++) if(str[j] == str[j-1]) return false;
+    return true;
+}
+
+int handleFile(const string &path){
+    auto file = openFile(path);
+    string line;
+    getline(file, line);
+    for(int i = 4; i < line.size(); i++){
+        string sub = line.substr(i-4, 4);
+        //cout << sub;
+        if(isUniqe(sub)) {return i;}
+        //cout <<' ' << sub << endl;
+    }
+    return 0;
+}
+
+int main()
+{
+    int result;
+    result = handleFile("example.txt");
+    cout << result;
+    if(result == expected){
+        cout << " \33[32m[OK]\33[39m\n";
+    }else{
+        cout << " != " << expected << " \33[31m[FAIL]\33[39m\n";
+    }
+    result = handleFile("input.txt");
+    cout << result;
+    clipboard(to_string(result));
+}
