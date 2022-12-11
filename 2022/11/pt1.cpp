0ab7047c20b679f99a1678715588e79cb22767e7
Avi <avi.ron012@gmail.com>
2022-12-11 15:15:14 +0200

    refactored 22/11

--- 2022/11/pt1.cpp
+++ 2022/11/pt1.cpp
@@ -3,40 +3,28 @@
 #include <algorithm>
 using namespace std;
 using namespace Aoc;
-
-using Test = bool (*)(int);
 using Action = int (*)(int);
 
+string expected = "10605";
 struct monkey
 {
     vector<int> items;
     Action action;
-    Test test;
+    int factor;
     int t;
     int f;
     int inspected = 0;
     auto operator<=>(const monkey &other) const {return this->inspected <=> other.inspected;};
 };
 
-
-string handleFile()
+string handleFile(vector<monkey> &monkeys)
 {
-    vector<monkey> monkeys = {
-        {vector<int>{91, 58, 52, 69, 95, 54}, [](int n){return n * 13;}, [](int n){return n % 7 == 0;}, 1, 5},
-        {vector<int>{80, 80, 97, 84}, [](int n){return n*n;}, [](int n){return n % 3 == 0;}, 3, 5},
-        {vector<int>{86, 92, 71}, [](int n){return n+7;}, [](int n){return n % 2 == 0;}, 0, 4},
-        {vector<int>{96, 90, 99, 76, 79, 85, 98, 61}, [](int n){return n+4;},[](int n){return n%11 == 0;}, 7, 6},
-        {vector<int>{60, 83, 68, 64, 73}, [](int n){return n * 19;}, [](int n){return n % 17 == 0;}, 1, 0},
-        {vector<int>{96, 52, 52, 94, 76, 51, 57}, [](int n){return n+3;}, [](int n){return n%5 == 0;}, 7, 3},
-        {vector<int>{75}, [](int n){return n + 5;}, [](int n){return n%13==0;}, 4, 2},
-        {vector<int>{83,75}, [](int n){return n + 1;}, [](int n){return n % 19 == 0;}, 2, 6}
-    };
     for(int i = 0; i < 20; i++){
         for(auto &m: monkeys){
             m.inspected += m.items.size();
             for(auto n: m.items){
                 int temp = m.action(n)/3;
-                if(m.test(temp)) monkeys[m.t].items.push_back(temp);
+                if(temp % m.factor == 0) monkeys[m.t].items.push_back(temp);
                 else monkeys[m.f].items.push_back(temp);
             }
             m.items = {};
@@ -48,7 +36,25 @@ string handleFile()
 
 int main()
 {
-    auto result = handleFile();
+    vector<monkey> example = {
+        {{79, 98}, [](int n){return n * 19;}, 23, 2, 3},
+        {{54, 65, 75, 74}, [](int n){return n + 6;}, 19, 2, 0},
+        {{79, 60, 97}, [](int n){return n * n;}, 13, 1, 3},
+        {{74}, [](int n){return n + 3;}, 17, 0, 1}
+    };
+    vector<monkey> input = {
+        {{91, 58, 52, 69, 95, 54}, [](int n){return n * 13;}, 7, 1, 5},
+        {{80, 80, 97, 84}, [](int n){return n*n;}, 3, 3, 5},
+        {{86, 92, 71}, [](int n){return n+7;}, 2, 0, 4},
+        {{96, 90, 99, 76, 79, 85, 98, 61}, [](int n){return n+4;}, 11, 7, 6},
+        {{60, 83, 68, 64, 73}, [](int n){return n * 19;}, 17, 1, 0},
+        {{96, 52, 52, 94, 76, 51, 57}, [](int n){return n+3;}, 5, 7, 3},
+        {{75}, [](int n){return n + 5;}, 13, 4, 2},
+        {{83,75}, [](int n){return n + 1;}, 19, 2, 6}
+    };
+    auto result = handleFile(example);
+    printResult(result, expected);
+    result = handleFile(input);
     cout << result;
     clipboard(result);
 }
